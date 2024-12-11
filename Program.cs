d15ec1215735c3f05946e62812d1f181187f3b83
Yosef-Gidaay2021 <yosefgiday2021@gmail.com>
2024-12-10 19:19:04 -0500

    Decorator pattern console app created, the necessary classes, abstarct classes and interfaces created

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a ConcreteComponent
+        IComponent<string> component = new ConcreteComponent();
+        Console.WriteLine("ConcreteComponent: " + component.GetText());
+
+        // Wrap the component in a PlainDecorator
+        IComponent<string> plainDecorator = new PlainDecorator(component);
+        Console.WriteLine("PlainDecorator: " + plainDecorator.GetText());
+
+        // Wrap the PlainDecorator in an UpperCaseDecorator
+        IComponent<string> upperCaseDecorator = new UpperCaseDecorator(plainDecorator);
+        Console.WriteLine("UpperCaseDecorator: " + upperCaseDecorator.GetText());
+
+        // Wrap the UpperCaseDecorator in a ColorDecorator
+        IComponent<string> colorDecorator = new ColorDecorator(upperCaseDecorator);
+        Console.WriteLine("ColorDecorator: " + colorDecorator.GetText());
+    }
+}
